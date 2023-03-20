aefb9df75f5fce0f40d79ed8c3a8a7604682203c
Israel Albino Galvan <israel.galvan@live.com>
2023-03-20 20:33:33 -0300

    Added CRUD functionalities and entity models for DevEvents and DevEventSpeaker

---
+++ AwesomeWebEvents.API/Entities/DevEvent.cs
@@ -0,0 +1,37 @@
+﻿using AwesomeWebEvents.API.Controllers;
+
+namespace AwesomeWebEvents.API.Entities
+{
+    public class DevEvent
+    {
+
+        public DevEvent() 
+        {
+            Speakers = new List<DevEventsSpeaker>();
+            IsDeleted = false;
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DevEventsSpeaker> Speakers { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public void update(string title, string description, DateTime startDate, DateTime endDate)
+        {
+            Title = title;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate; 
+        }
+
+        public void Delete() 
+        {
+            IsDeleted = true;
+        }
+
+
+    }
+}
---
+++ AwesomeWebEvents.API/Persistence/DevEventsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeWebEvents.API.Entities;
+
+namespace AwesomeWebEvents.API.Persistence
+{
+    public class DevEventsDbContext
+    {
+        public List<DevEvent> DevEvents { get; set; }
+
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
